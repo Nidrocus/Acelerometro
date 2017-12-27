097e15783f73a830f0b6347de9bec95f6bb4f6d3
fedetorres <federico.torres@infinixsoft.com>
2017-12-27 10:37:35 -0300

    linkeados los modulos, test de proximidad

---
+++ acelerometro/src/main/java/com/nidrocus/acelerometro/ProximitySensorManager.kt
@@ -0,0 +1,58 @@
+package com.nidrocus.acelerometro
+
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.util.Log
+
+/**
+ * Created by Federico Torres on 27/12/2017.
+ */
+class ProximitySensorManager  constructor(val context : Context) {
+
+    val sensorManager : SensorManager = context.getSystemService(Context.SENSOR_SERVICE) as (SensorManager)
+    val TAG =  "ProximitySensor"
+    var sensor : Sensor?
+    var proximityListener : SensorEventListener? = null
+
+
+    init {
+    sensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY)
+     Log.i(TAG,"Sensor no disponible")
+    }
+
+
+    fun listenUpdates(onUpdate: (event: SensorEvent?) -> Any ) {
+        proximityListener = object : SensorEventListener {
+          override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+
+          }
+
+          override fun onSensorChanged(event: SensorEvent?) {
+            val maxRange = sensor?.maximumRange
+              if(event != null && maxRange != null){
+                  if (event.values[0]< maxRange){
+                      //si se detecta un objeto mas cerca del maximo alcance del sensor entonces hay un objeto en el medio
+                      onUpdate(event)
+                  }
+              }
+
+
+          }
+      }
+
+        sensorManager.registerListener(proximityListener,sensor,2*1000 *1000)
+    }
+
+
+    fun stopUpdates(){
+        if(proximityListener != null){
+        sensorManager.unregisterListener(proximityListener)
+        proximityListener = null
+        }
+    }
+
+
+}
--- app/src/main/java/com/nidrocus/rapidoyfurioso/MainActivity.kt
+++ app/src/main/java/com/nidrocus/rapidoyfurioso/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.nidrocus.rapidoyfurioso
 
+import android.content.Context
+import android.graphics.Color
+import android.hardware.SensorEvent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.nidrocus.acelerometro.ProximitySensorManager
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+       val sensorManager =  ProximitySensorManager(this)
+        sensorManager.listenUpdates { event: SensorEvent? -> {
+            window.decorView.setBackgroundColor(Color.RED)
+        }}
+
     }
 }
