c74f100a18328c2eb5fb5c0aa6f4490c03269370
Cristian <cristiangarcia@infinixsoft.com>
2018-01-04 19:08:57 -0300

    se agrego velocidad maxima

--- app/src/main/java/com/nidrocus/rapidoyfurioso/MainActivity.kt
+++ app/src/main/java/com/nidrocus/rapidoyfurioso/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.TextView
 import com.nidrocus.acelerometro.GyroscopeSensor
 import com.nidrocus.acelerometro.SensorWrapper
+import com.nidrocus.customviews.Velocimetro
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -12,20 +13,20 @@ class MainActivity : AppCompatActivity() {
     val TAG = "MainActivity"
     var sensorWrapper: SensorWrapper? = null
     var sensorFede: GyroscopeSensor? = null
+    var velocimetro: Velocimetro? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_velocimetro)
 
+        velocimetro = findViewById(R.id.velocimetro)
         //sensorWrapper = SensorWrapper(this, Sensor.TYPE_GYROSCOPE ,this::onRotationUpdate)
         sensorFede = GyroscopeSensor(this)
-        sensorFede?.listenUpdates(this::onRotationUpdate,this::onOrientationUpdate)
-
-
+        sensorFede?.listenUpdates(this::onRotationUpdate, this::onOrientationUpdate)
+        velocimetro?.setMaxVelocity(3f)
     }
 
-
-    private fun onOrientationUpdate(orientation: FloatArray){
+    private fun onOrientationUpdate(orientation: FloatArray) {
         val tvOrientation = findViewById<TextView>(R.id.tv_orientation)
         tvOrientation.text = "x: ${orientation[0]}, y:${orientation[1]}, z:${orientation[2]} NN: ${orientation[3]}"
     }
@@ -35,11 +36,12 @@ class MainActivity : AppCompatActivity() {
         tv_y.text = "y: %.2f".format(y)
         tv_z.text = "z: %.2f".format(z)
 
+        val angular = Math.sqrt((x * x + y * y + z * z).toDouble())
         val tvAngular = findViewById<TextView>(R.id.tv_angular)
-        tvAngular.text = "angular: %.2f".format( Math.sqrt((x*x+y*y+z*z).toDouble()))
+        tvAngular.text = "angular: %.2f".format(angular)
+
+        velocimetro?.setVelocity(angular.toFloat())
     }
-
-
 
     override fun onDestroy() {
         super.onDestroy()
--- customviews/src/main/java/com/nidrocus/customviews/Velocimetro.kt
+++ customviews/src/main/java/com/nidrocus/customviews/Velocimetro.kt
@@ -5,9 +5,10 @@ import android.util.AttributeSet
 import android.view.Gravity
 import android.view.View
 import android.view.animation.Animation
-import android.view.animation.BounceInterpolator
+import android.view.animation.LinearInterpolator
 import android.widget.FrameLayout
 import android.view.animation.RotateAnimation
+import java.util.concurrent.TimeUnit
 
 /**
  * Created by cristian on 28/12/2017.
@@ -16,12 +17,11 @@ class Velocimetro @JvmOverloads constructor(context: Context, attrs: AttributeSe
         FrameLayout(context, attrs, defStyleAttr) {
 
     private val manecilla: View
+    private var maxVelocity: Float = 0f
 
     init {
         manecilla = Manecilla(context)
-        val paramsBackground = FrameLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT)
-        addView(VelocimetroBackground(context), paramsBackground)
-
+        addView(VelocimetroBackground(context), FrameLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT))
         viewTreeObserver.addOnGlobalLayoutListener(this::onGlobalLayout)
     }
 
@@ -32,17 +32,33 @@ class Velocimetro @JvmOverloads constructor(context: Context, attrs: AttributeSe
         params.marginStart = -params.width / 2
 
         if (manecilla.parent == null) addView(manecilla, params)
+
+        manecilla.pivotX = manecilla.width.toFloat()
+        manecilla.pivotY = manecilla.height / 2f
     }
 
-    fun updatePosition(angle: Float) {
-        val anim = RotateAnimation(manecilla.rotation, angle, Animation.RELATIVE_TO_SELF,
-                1f, Animation.RELATIVE_TO_SELF, 0.5f)
+    private fun updatePosition(angle: Float) {
+        manecilla.rotation = angle
 
-        anim.interpolator = BounceInterpolator()
-        anim.duration = 1000
-        anim.isFillEnabled = true
-        anim.fillAfter = true
+//        val anim = RotateAnimation(manecilla.rotation, angle, Animation.RELATIVE_TO_SELF,
+//                1f, Animation.RELATIVE_TO_SELF, 0.5f)
+//
+//        anim.interpolator = LinearInterpolator()
+//        //anim.duration = 1000
+//        anim.isFillEnabled = true
+//        anim.fillAfter = true
+//
+//        manecilla.startAnimation(anim)
+    }
 
-        manecilla.startAnimation(anim)
+    fun setMaxVelocity(maxVelocity: Float) {
+        this.maxVelocity = maxVelocity
+    }
+
+    fun setVelocity(velocity: Float) {
+        if (velocity > 0 && velocity < maxVelocity) {
+            val percentage = velocity * 100 / maxVelocity
+            updatePosition(percentage * 100 / 180f)
+        }
     }
 }
